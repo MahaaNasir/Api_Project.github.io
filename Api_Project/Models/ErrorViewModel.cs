b160a15806aef107cd7fd09d657b948276554669
MahaaNasir <mahanasir25392@gmail.com>
2024-03-16 04:28:07 -0400

    Add project files.

---
+++ Api_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Api_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
